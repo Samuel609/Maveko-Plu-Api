2d8f93cb4f0c4bfe6c6e3734460c6d43327e8b50
Samuel Fita <samabera03@gmail.com>
2023-07-04 22:28:26 +0300

    fixed some errors

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,43 +1,43 @@
 class CurrenciesController < ApplicationController
     def index
         @currencies = Currencies.all 
-        render json: {data: @currencies, success: true, status: 200}
+        render json: {data: @currency, success: true, status: 200}
     end
 
     def show
-        @currencies = Currencies.find(params[:id])
-        render json: {data: @currencies, success: true, status: 200}
+        @currency = Currencies.find(params[:id])
+        render json: {data: @currency, success: true, status: 200}
     end
 
     def create
-        @currencies = Currencies.new(currencies_params)
-        if @currencies.save
-            render json: {data: @currencies, success: true, status: 200}
+        @currency = Currencies.new(currency_params)
+        if @currency.save
+            render json: {data: @currency, success: true, status: 200}
         else
-            render json: {data: @currencies.errors, success: false, status: 400}
+            render json: {data: @currency.errors, success: false, status: 400}
         end
     end
 
     def update
-        @currencies = Currencies.find(params[:id])
-        if @currencies.update(currencies_params)
-            render json: {data: @currencies, success: true, status: 200}
+        @currency = Currencies.find(params[:id])
+        if @currency.update(currency_params)
+            render json: {data: @currency, success: true, status: 200}
         else
-            render json: {data: @currencies.errors, success: false, status: 400}
+            render json: {data: @currency.errors, success: false, status: 400}
         end
     end
 
     def destroy
-        @currencies = Currencies.find(params[:id])
-        if @currencies.destroy()
-            render json: {data: currencies, success: true, status: 200}
+        @currency = Currencies.find(params[:id])
+        if @currency.destroy()
+            render json: {data: currency, success: true, status: 200}
         else
-            render json: {data: currencies.errors, success: false, status: 400}
+            render json: {data: currency.errors, success: false, status: 400}
         end
     end
 
     private
-    def currencies_params
-        params.require(:currencies).permit(:name, :symbol)
+    def currency_params
+        params.require(:currency).permit(:name, :symbol)
     end    
 end
--- app/controllers/pricings_controller.rb
+++ app/controllers/pricings_controller.rb
@@ -1,43 +1,43 @@
 class PricingsController < ApplicationController
     def index
         @pricings = Pricings.all 
-        render json: {data: @pricings, success: true, status: 200}
+        render json: {data: @pricing, success: true, status: 200}
     end
 
     def show
-        @pricings = Pricings.find(params[:id])
-        render json: {data: @pricings, success: true, status: 200}
+        @pricing = Pricings.find(params[:id])
+        render json: {data: @pricing, success: true, status: 200}
     end
 
     def create
-        @pricings = Pricings.new(pricings_params)
-        if @pricings.save
-            render json: {data: @pricings, success: true, status: 200}
+        @pricing = Pricings.new(pricing_params)
+        if @pricing.save
+            render json: {data: @pricing, success: true, status: 200}
         else
-            render json: {data: @pricings.errors, success: false, status: 400}
+            render json: {data: @pricing.errors, success: false, status: 400}
         end
     end
 
     def update
-        @pricings = Pricings.find(params[:id])
-        if @pricings.update(pricings_params)
-            render json: {data: @pricings, success: true, status: 200}
+        @pricing = Pricings.find(params[:id])
+        if @pricing.update(pricing_params)
+            render json: {data: @pricing, success: true, status: 200}
         else
-            render json: {data: @pricings.errors, success: false, status: 400}
+            render json: {data: @pricing.errors, success: false, status: 400}
         end
     end
 
     def destroy
-        @pricings = Pricings.find(params[:id])
-        if @pricings.destroy()
-            render json: {data: pricings, success: true, status: 200}
+        @pricing = Pricings.find(params[:id])
+        if @pricing.destroy()
+            render json: {data: pricing, success: true, status: 200}
         else
-            render json: {data: pricings.errors, success: false, status: 400}
+            render json: {data: pricing.errors, success: false, status: 400}
         end
     end
 
     private
-    def pricings_params
-        params.require(:pricings).permit(:price_in_euro, :old_purchase_price, :valid_from_old_purchase, :new_purchase_price, :valid_from_new_purchase, :valid_to_new_purchase, :old_retail_price, :new_retail_price, :new_retail_price_valid_from, :new_retail_price_valid_to, :change_in_percentage)
+    def pricing_params
+        params.require(:pricing).permit(:price_in_euro, :old_purchase_price, :valid_from_old_purchase, :new_purchase_price, :valid_from_new_purchase, :valid_to_new_purchase, :old_retail_price, :new_retail_price, :new_retail_price_valid_from, :new_retail_price_valid_to, :change_in_percentage)
     end
 end
