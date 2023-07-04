58966733da5a3b3f1a9163d2477e4af2dfa37fdb
nigussolomon <nigussolomon444@gmail.com>
2023-07-04 21:25:52 +0300

    configured controller and route and all tests passed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :unit_of_measures
   # Defines the root path route ("/")
   # root "articles#index"
 end
