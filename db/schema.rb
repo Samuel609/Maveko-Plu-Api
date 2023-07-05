67fa9d9b9d438aa1178b1171df488cbedcdf010e
nigussolomon <nigussolomon444@gmail.com>
2023-07-06 00:02:05 +0300

    fixed schema issues

--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_181953) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "aas", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "abs", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "currencies", force: :cascade do |t|
     t.string "name"
     t.string "symbol"
