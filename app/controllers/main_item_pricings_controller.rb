f7c3a2f052713b3b4ee79203d0a387fbde71371c
nigussolomon <nigussolomon444@gmail.com>
2023-07-06 16:12:14 +0300

    implemented ransact searching

---
+++ app/controllers/main_item_pricings_controller.rb
@@ -0,0 +1,13 @@
+class MainItemPricingsController < ApplicationController
+  def index
+    @main_item_pricings = MainItemPricing.all
+    render json: {data: @main_item_pricings, status: 200, Success: true}, status: :ok
+  end
+
+  def filter
+    @q = MainItemPricing.ransack(params[:q])
+    @main_item_pricings = @q.result(distinct: true)
+    
+    render json: { data: @main_item_pricings, status: 200, success: true }, status: :ok
+  end
+end
