7cbbd65cc13ef988e272f1ee1a660c0ca9876682
nonyx777 <nahomretebo@gmail.com>
2023-07-07 08:59:26 +0300

    price update for supplier implemented

--- app/controllers/pricings_controller.rb
+++ app/controllers/pricings_controller.rb
@@ -1,6 +1,7 @@
 class PricingsController < ApplicationController
     def index
         @pricings = Pricing.all 
+        updatePrice(13, 1)
         render json: {data: @pricings, success: true, status: 200}, status: :ok
     end
 
@@ -40,4 +41,25 @@ class PricingsController < ApplicationController
     def pricing_params
         params.require(:pricing).permit(:price_in_euro, :old_purchase_price, :valid_from_old_purchase, :new_purchase_price, :valid_from_new_purchase, :valid_to_new_purchase, :old_retail_price, :new_retail_price, :new_retail_price_valid_from, :new_retail_price_valid_to, :change_in_percentage)
     end
+end
+
+updatePending = Array.new
+
+#price update function
+def updatePrice(updated_price, supplierItemPriceID)
+    #takes the price to be updated
+    #updates it in the supplierItemPriceID
+    supplierItemPrice = SupplierItemPricing.find(supplierItemPriceID)
+    supplierPricing = supplierItemPrice.pricing
+    #saving the the current price
+    old_price = supplierPricing.new_purchase_price
+    supplierPricing.new_purchase_price = updated_price
+    supplierPricing.old_purchase_price = old_price
+    supplierPricing.save
+
+    #return supplierPricing.id
+end
+#approval function
+def approvalFunction(approved)
+    
 end
