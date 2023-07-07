6a7d568d66759ced3efc49b40a7f4c1706049bea
nigussolomon <nigussolomon444@gmail.com>
2023-07-07 04:38:13 +0300

    linked the user model with customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_07_013422) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_07_013635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_013422) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_customers_on_user_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -119,6 +121,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_013422) do
   add_foreign_key "customer_item_pricings", "customers"
   add_foreign_key "customer_item_pricings", "items"
   add_foreign_key "customer_item_pricings", "pricings"
+  add_foreign_key "customers", "users"
   add_foreign_key "items", "unit_of_measures", column: "base_unit_id"
   add_foreign_key "items", "unit_of_measures", column: "target_unit_id"
   add_foreign_key "main_item_pricings", "items"
