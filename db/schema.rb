357cfe3374071a54a6947d68c0b80e83c9d563db
nigussolomon <nigussolomon444@gmail.com>
2023-07-07 04:36:07 +0300

    added role field to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_135529) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_07_013422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -113,6 +113,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_135529) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role", default: "user", null: false
   end
 
   add_foreign_key "customer_item_pricings", "customers"
