4ee201ce6b278ee5d53f52d706444a7f1f7b556c
nigussolomon <nigussolomon444@gmail.com>
2023-07-11 09:35:38 +0300

    migrated to the shared requests testing

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  include_examples('request_shared_spec', 'users', 2)
+
+  let(:valid_attributes) do
+    {
+      email: Faker::Internet.email,
+      password: "pass123",
+      password_confirmation: "pass123"
+    }
+  end
+
+  let(:invalid_attributes) do 
+    {
+      email: Faker::Internet.email,
+      password: nil,
+      password_confirmation: "XXXXXXY"
+    }
+  end
+
+  let(:new_attributes) do 
+    {
+      email: Faker::Internet.email,
+      password: "XXXXXXX",
+      password_confirmation: "XXXXXXX"
+    }
   end
 end
