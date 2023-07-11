fa1def2b32d4670e82126b689b24a23416477788
nigussolomon <nigussolomon444@gmail.com>
2023-07-11 09:32:29 +0300

    adjusted controllers to use common controller

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,43 +1,8 @@
 class CurrenciesController < ApplicationController
-    def index
-        @currencies = Currency.all 
-        render json: {data: @currency, success: true, status: 200}, status: :ok
-    end
-
-    def show
-        @currency = Currency.find(params[:id])
-        render json: {data: @currency, success: true, status: 200}, statu: :ok
-    end
-
-    def create
-        @currency = Currency.new(currency_params)
-        if @currency.save
-            render json: {data: @currency, success: true, status: 200}, status: :created
-        else
-            render json: {data: @currency.errors, success: false, status: 400}, status: :unprocessable_entity
-        end
-    end
-
-    def update
-        @currency = Currency.find(params[:id])
-        if @currency.update(currency_params)
-            render json: {data: @currency, success: true, status: 200}, status: :ok
-        else
-            render json: {data: @currency.errors, success: false, status: 400}, status: :unprocessable_entity
-        end
-    end
-
-    def destroy
-        @currency = Currency.find(params[:id])
-        if @currency.destroy()
-            render json: {data: currency, success: true, status: 200}, status: :ok
-        else
-            render json: {data: currency.errors, success: false, status: 400}, status: :unprocessable_entity
-        end
-    end
+    include Common
 
     private
-    def currency_params
-        params.require(:currency).permit(:name, :symbol)
+    def model_params
+        params.require(:payload).permit(:name, :symbol, :currency_id)
     end    
 end
--- app/controllers/pricings_controller.rb
+++ app/controllers/pricings_controller.rb
@@ -1,48 +1,8 @@
 class PricingsController < ApplicationController
-    before_action :set_price_saved, only: [:updatePrice, :updateApproval]
-    def index
-        @pricings = Pricing.all
-        render json: {data: @pricings, success: true, status: 200}, status: :ok
-    end
-    
-    def show
-        @pricing = Pricing.find(params[:id])
-        render json: {data: @pricing, success: true, status: 200}, status: :ok
-    end
-
-    def create
-        @pricing = Pricing.new(pricing_params)
-        if @pricing.save
-            render json: {data: @pricing, success: true, status: 200}, status: :created
-        else
-            render json: {data: @pricing.errors, success: false, status: 400}, status: :unprocessable_entity
-        end
-    end
-
-    def update
-        @pricing = Pricing.find(params[:id])
-        if @pricing.update(pricing_params)
-            render json: {data: @pricing, success: true, status: 200}, status: :ok
-        else
-            render json: {data: @pricing.errors, success: false, status: 400}, status: :unprocessable_entity
-        end
-    end
-
-    def destroy
-        @pricing = Pricing.find(params[:id])
-        if @pricing.destroy()
-            render json: {data: pricing, success: true, status: 200}, status: :ok
-        else
-            render json: {data: pricing.errors, success: false, status: 400}, status: :unprocessable_entity
-        end
-    end
+    include Common
 
     private
-    def pricing_params
-        params.require(:pricing).permit(:price_in_euro, :old_purchase_price, :valid_from_old_purchase, :new_purchase_price, :valid_from_new_purchase, :valid_to_new_purchase, :old_retail_price, :new_retail_price, :new_retail_price_valid_from, :new_retail_price_valid_to, :change_in_percentage)
-    end
-    
-    def set_price_saved
-        @priceSaved = priceSaved
+    def model_params
+        params.require(:payload).permit(:price_in_euro, :old_purchase_price, :valid_from_old_purchase, :new_purchase_price, :valid_from_new_purchase, :valid_to_new_purchase, :old_retail_price, :new_retail_price, :new_retail_price_valid_from, :new_retail_price_valid_to, :change_in_percentage, :currency_id)
     end
 end
